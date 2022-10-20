1cd1e229373f03dc21db0194934a8c8f4ef6960a
Omar Salem <omarsalem0721@gmail.com>
2022-10-20 20:10:59 +0200

    Add preview component

--- src/components/Product/Preview/Preview.tsx
+++ src/components/Product/Preview/Preview.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import styles from './Preview.module.css';
+import mainImg from '../../../assets/mainProduct.png';
+import Img1 from '../../../assets/image1.png';
+import Img2 from '../../../assets/image2.png';
+import Img3 from '../../../assets/image3.png';
 
 const Preview = () => {
   return (
-    <div>Preview</div>
-  )
-}
+    <div className={styles.preview}>
+      <div>
+        <img src={mainImg} draggable={false} alt="main preview product" />
+      </div>
+      <div className={styles.subImgs}>
+        <img draggable={false} src={Img1} alt="img1" />
+        <img draggable={false} src={Img2} alt="img2" />
+        <img draggable={false} src={Img3} alt="img3" />
+      </div>
+    </div>
+  );
+};
 
-export default Preview
+export default Preview;
